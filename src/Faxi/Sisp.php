7c705c2679c5d854feebc3c101ca26f090774366
Assis Ferreira <ferreira.assisruny@gmail.com>
2022-03-25 18:27:00 -0100

    start

---
+++ src/Faxi/Sisp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Faxi;
+
+class Sisp
+{
+    public $posId;
+    public $posAuthCode;
+
+    public $apiBaseUrl = "https://mc.vinti4net.cv/BizMPIOnUsSisp/";
+
+    function __construct($pos, $authCode, $apiUrl = null)
+    {
+        
+    }
+
+    function buyForm()
+    {
+        
+    }
+
+}
+
+?>
